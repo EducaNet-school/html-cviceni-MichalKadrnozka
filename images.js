8ab5639b7f4fa2c89ca6e265c67ca7f65ecf2cf6
MichalKadrnozka <michal.kadrnozka@educanet.cz>
2022-10-13 11:50:37 +0200

    update

---
+++ images.js
@@ -0,0 +1,56 @@
+
+let img = 'img1'
+function img_close(){
+    document.getElementById(img).className = 'img_close'
+    document.getElementById('button').className = 'close_button'
+}
+function img1_open(){
+    document.getElementById('img1').className = 'img1'
+    document.getElementById('button').className = 'close_button_visible'
+    img = 'img1'
+}
+function img2_open(){
+    document.getElementById('img2').className = 'img2'
+    document.getElementById('button').className = 'close_button_visible'
+    img = 'img2'
+}
+function img3_open(){
+    document.getElementById('img3').className = 'img3'
+    document.getElementById('button').className = 'close_button_visible'
+    img = 'img3'
+}
+function img4_open(){
+    document.getElementById('img4').className = 'img4'
+    document.getElementById('button').className = 'close_button_visible'
+    img = 'img4'
+}
+function img5_open(){
+    document.getElementById('img5').className = 'img5'
+    document.getElementById('button').className = 'close_button_visible'
+    img = 'img5'
+}
+function img6_open(){
+    document.getElementById('img6').className = 'img6'
+    document.getElementById('button').className = 'close_button_visible'
+    img = 'img6'
+}
+function img7_open(){
+    document.getElementById('img7').className = 'img7'
+    document.getElementById('button').className = 'close_button_visible'
+    img = 'img7'
+}
+function img8_open(){
+    document.getElementById('img8').className = 'img8'
+    document.getElementById('button').className = 'close_button_visible'
+    img = 'img8'
+}
+function img9_open(){
+    document.getElementById('img9').className = 'img9'
+    document.getElementById('button').className = 'close_button_visible'
+    img = 'img9'
+}
+function img10_open(){
+    document.getElementById('img10').className = 'img10'
+    document.getElementById('button').className = 'close_button_visible'
+    img = 'img10'
+}
